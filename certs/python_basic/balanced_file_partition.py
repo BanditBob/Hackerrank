72a949024f2f47bd8500858d203028aca1d90e3d
BanditBob <brandon.bennett.ai@gmail.com>
2024-10-13 22:18:31 -0600

    updated formatting

--- certs/python_basic/balanced_file_partition.py
+++ certs/python_basic/balanced_file_partition.py
@@ -23,14 +23,18 @@ if __name__ == "__main__":
     fptr = open(os.environ["OUTPUT_PATH"], "w")
     parent_count = int(input().strip())
     parent = []
+
     for _ in range(parent_count):
         parent_item = int(input().strip())
         parent.append(parent_item)
+
     files_size_count = int(input().strip())
     files_size = []
+
     for _ in range(files_size_count):
         files_size_item = int(input().strip())
         files_size.append(files_size_item)
+
     result = mostBalancedPartition(parent, files_size)
     fptr.write(str(result) + "\n")
     fptr.close()
